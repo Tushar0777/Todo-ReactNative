4936e50236b81183899765abd9963c21a6769c95
Tushar <tusharpal.dev.in@gmail.com>
2025-11-02 00:36:21 +0530

    created a basic todo app in react native

---
+++ Backend/index.js
@@ -0,0 +1,59 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import pkg from 'pg';
+const { Pool } = pkg;
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const app=express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const pool=new Pool({
+    user:'postgres',
+    host:process.env.IP_ADDRESS,
+    database:'todoapp',
+    password:process.env.DB_PASSWORD,
+    post:5432,
+});
+
+//to check sab theek hai na 
+pool.query("SELECT NOW()", (err, res) => {
+  if (err) console.error("DB error:", err);
+  else console.log("Connected successfully:", res.rows);
+ 
+});
+
+
+app.get("/tasks", async (req, res) => {
+  const result = await pool.query("SELECT * FROM tasks ORDER BY id DESC");
+  res.json(result.rows);
+});
+
+app.post("/tasks", async (req, res) => {
+  const { title, description } = req.body;
+  const result = await pool.query(
+    "INSERT INTO tasks (title, description) VALUES ($1, $2) RETURNING *",
+    [title, description]
+  );
+  res.json(result.rows[0]);
+    console.log("Task added:", res.data);
+});
+
+app.put("/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+  const { completed } = req.body;
+  await pool.query("UPDATE tasks SET completed=$1 WHERE id=$2", [completed, id]);
+  res.sendStatus(200);
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+  await pool.query("DELETE FROM tasks WHERE id=$1", [id]);
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
